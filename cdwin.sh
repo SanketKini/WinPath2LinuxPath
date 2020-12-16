1a570faea42b2b55869cc19de566fa4334bafa5f
Sanket Kini <sanket.kini@atos.net>
2020-12-16 09:00:42 +0000

    Added in code comments

--- cdwin.sh
+++ cdwin.sh
@@ -2,16 +2,17 @@
 line=$1
 if [ ! -z "$line" ]
 then
-	line=${line//\\//}
+	## Convert Drive Letter to LowerCase
 	drive=${line:0:1}
 	drive=${drive,,}
+	## Replace "\" with "/" and " " with "\ "
 	line_mf=${line:2:${#line}}
 	line_mf=${line_mf//\\//}
 	line_mf=${line_mf// /\\ }
 	bashPath="/cygdrive/$drive$line_mf"
 	wslPath="/mnt/$drive$line_mf"
 	echo "For Cygwin: $bashPath"
-	#echo "For WSL: $wslPath"
+	echo "For WSL: $wslPath"
 else
 	echo "Please enter correct value"
 fi
